31ee9addb48f327473b919d314a01bd116434264
Lincoln Bhattachan <bhattachanl17@gmail.com>
2022-09-09 11:04:34 +0545

    addeddesiredCap

--- Mobile Automation/Test.py
+++ Mobile Automation/Test.py
@@ -3,8 +3,13 @@ from appium import webdriver
 desired_cap ={
   "deviceName": "1B11293210NA103R",
   "platformName": "Android",
-  "app": "D:\\freelance\\pytest\\Mobile Automation\\apk\\com.android.messaging_1.0.001-10001140_minAPI30(nodpi)_apkmirror.com.apk"
+  "app": "D:\\freelance\\pytest\\Mobile Automation\\apk\\com.android.messaging_1.0.001-10001140_minAPI30(nodpi)_apkmirror.com.apk",
+  "automationName": "UiAutomator2",
+  "appPackage": "com.android.messaging",
+  "appActivity": "com.android.messaging.ui.conversationlist.ConversationListActivity"
 }
 
 driver= webdriver.Remote("http://localhost:4723/wd/hub",desired_cap)
+
+
  
