7535c00810b4c8e429b035c213e79f518b9796e0
Lincoln Bhattachan <bhattachanl17@gmail.com>
2022-09-10 18:48:46 +0545

    minorchanges

--- Mobile Automation/SendSMS.py
+++ Mobile Automation/SendSMS.py
@@ -1,10 +1,7 @@
-
+#importing modules and packages
 from appium import webdriver
 from selenium.webdriver.common.by import By
-
-import time
-receiver_number='9840449049'
-
+#list of desired capabilities
 desired_capabilities = {
     "deviceName": "emulator-5554",
     "platformName": "Android",
@@ -12,10 +9,15 @@ desired_capabilities = {
     "appPackage": "com.android.mms",
     "appActivity": "com.android.mms.ui.ComposeMessageActivity"
 }
+#conneting with appium server
 driver = webdriver.Remote("http://localhost:4723/wd/hub", desired_capabilities)
+
+#findind elements and automating
 driver.find_element(By.ID,'com.android.mms:id/recipients_editor').send_keys('98404409049')
-driver.find_element(By.ID,'com.android.mms:id/embedded_text_editor').send_keys('Hello My name is Lincoln Bhattachan')
+driver.find_element(By.ID,'com.android.mms:id/embedded_text_editor').send_keys('Good Morning')
 driver.find_element(By.ID,'com.android.mms:id/send_button_sms').click()
 driver.quit()
+
+#exit
 
 
