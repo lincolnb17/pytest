0b7e3dde2ef4bffa8bcbeca65087a4a0aa36468d
Lincoln Bhattachan <bhattachanl17@gmail.com>
2022-09-11 10:57:17 +0545

    screenshotforrealdevices

--- Mobile Automation/installingApk.py
+++ Mobile Automation/installingApk.py
@@ -3,7 +3,7 @@ from appium import webdriver
 desired_cap ={
   "deviceName": "emulator-5554",
   "platformName": "Android",
-  "app": "D:\\freelance\\pytest\\Mobile Automation\\apk\\com.android.messaging_1.0.001-10001140_minAPI30(nodpi)_apkmirror.com.apk",  #location of apk file to be installed
+  "app": "D:\\freelance\\pytest\\Mobile Automation\\apk\\message.apk",  #location of apk file to be installed
   "automationName": "UiAutomator2",
 }
 driver= webdriver.Remote("http://localhost:4723/wd/hub",desired_cap)
