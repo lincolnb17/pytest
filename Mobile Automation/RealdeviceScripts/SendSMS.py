66e5a4ff4f77644786e3a2ccf2c71b541bbf6411
Lincoln Bhattachan <bhattachanl17@gmail.com>
2022-09-12 12:45:18 +0545

    finalProject

--- Mobile Automation/RealdeviceScripts/SendSMS.py
+++ Mobile Automation/RealdeviceScripts/SendSMS.py
@@ -6,17 +6,18 @@ desired_capabilities = {
     "deviceName": "1B11293210NA103R",
     "platformName": "Android",
     "automationName": "uiautomator2",
-    "appPackage": "com.simplemobiletools.smsmessenger",
-    "appActivity": "com.simplemobiletools.smsmessenger.activities.NewConversationActivity"
+    "appPackage": "com.android_coding.sendmessage",
+    "appActivity": "com.android_coding.sendmessage.MainActivity",
+    "autoGrantPermissions":  "true"
 }
 #conneting with appium server
 driver = webdriver.Remote("http://localhost:4723/wd/hub", desired_capabilities)
 
 #findind elements and automating
-driver.find_element(By.ID,'com.simplemobiletools.smsmessenger:id/new_conversation_address').send_keys('98404409049')
-driver.find_element(By.ID,'com.simplemobiletools.smsmessenger:id/new_conversation_confirm').click()
-driver.find_element(By.ID,'com.simplemobiletools.smsmessenger:id/thread_type_message').send_keys('Good Morning')
-driver.find_element(By.ID,'com.simplemobiletools.smsmessenger:id/thread_send_message').click()
+driver.find_element(By.ID,'com.android_coding.sendmessage:id/et_phone').send_keys('9840449049')
+driver.find_element(By.ID,'com.android_coding.sendmessage:id/et_message').send_keys('Good Morning')
+driver.find_element(By.ID,'com.android_coding.sendmessage:id/bt_send').click()
+
 
 
 
