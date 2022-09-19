b2b96c79b6e05792208eaaaea09900dd16e54ec8
lincolnb17 <bhattachanl17@gmail.com>
2022-09-19 23:16:19 +0545

    final

--- Mobile Automation/Final_script/sendingAndReceving.py
+++ Mobile Automation/Final_script/sendingAndReceving.py
@@ -8,19 +8,20 @@ import time
 from appium import webdriver
 from selenium.webdriver.common.by import By
 import os
-
 #sending information
 phone_no ='9821322643'
 text='Good Morning'
 
 #staring appium server at PORT=4723
 os.system("start /B start cmd.exe @cmd /k appium")
+
+#desired_capabilities of the app
+path = "D:\\freelance\\pytest\\Mobile Automation\\apk\\verizon.apk"  ##path of the apk file
+appPackage = "com.verizon.messaging.vzmsgs"
+appActivity = "com.verizon.mms.ui.ComposeMessageActivity"
 def testsendsms():
     # device and app details
     deviceId = "4c59e4b60406"
-    path = "D:\\freelance\\pytest\\Mobile Automation\\apk\\verizon.apk"  ##path of the apk file
-    appPackage = "com.verizon.messaging.vzmsgs"
-    appActivity = "com.verizon.mms.ui.ComposeMessageActivity"
 
     desired_capabilities = {
         "deviceName": deviceId,  # emulator id
@@ -33,7 +34,11 @@ def testsendsms():
         "skipDeviceInitialization": "true"
     }
      # conneting with appium server
-    driver = webdriver.Remote("http://localhost:4723/wd/hub",desired_capabilities)
+    try:
+        driver = webdriver.Remote("http://localhost:4723/wd/hub",desired_capabilities)
+    except:
+        print("Appium Server is not running at PORT= 4723")
+
     driver.find_element(By.ID, 'com.verizon.messaging.vzmsgs:id/start').click()
     time.sleep(1)
     driver.find_element(By.ID, 'com.verizon.messaging.vzmsgs:id/skip_provisioning_tv').click()
@@ -53,10 +58,6 @@ os.system("start /B start cmd.exe @cmd /k appium -a 0.0.0.0 -p 4724")
 def testreceivesms():
     #device and app details
     deviceId2="1B11293210NA103R"
-    path = "D:\\freelance\\pytest\\Mobile Automation\\apk\\verizon.apk"  ##path of the apk file
-    appPackage="com.verizon.messaging.vzmsgs"
-    appActivity="com.verizon.mms.ui.ComposeMessageActivity"
-
 
     desired_capabilities = {
         "deviceName": deviceId2, #emulator id
@@ -70,7 +71,10 @@ def testreceivesms():
     }
 
     # conneting with appium server
-    driver = webdriver.Remote("http://localhost:4724/wd/hub",desired_capabilities)
+    try:
+        driver = webdriver.Remote("http://localhost:4724/wd/hub",desired_capabilities)
+    except:
+        print("Appium Server is not running at PORT=4723")
 
     driver.find_element(By.ID, 'com.verizon.messaging.vzmsgs:id/start').click()
     time.sleep(1)
